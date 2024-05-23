58498c7dad71e2da85b6b534ff50292f1050a6e9
Jeppe Stenstrup <jepstrup@gmail.com>
2024-05-23 08:48:27 +0200

    format: Formatted spacing

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,22 +1,16 @@
 -- Themes
 return {
 	"typicode/bg.nvim",
-
 	"ellisonleao/gruvbox.nvim",
-
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 	},
-
 	{
 		"rose-pine/nvim",
 		name = "rose-pine",
 	},
-
 	"sainnhe/everforest",
-
 	"savq/melange-nvim",
-
 	"folke/tokyonight.nvim",
 }
